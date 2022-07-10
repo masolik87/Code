d37394ec2051a0d0887712c9c463baa956c10bc0
Дмитрий <dimon20.07@mail.ru>
2022-07-11 00:44:31 +0300

    If-Else

---
+++ Ex5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
